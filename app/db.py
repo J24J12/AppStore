aca2b1c459eff9ec0e8080973ba68267c2c342e2
jaegarpoon <73332327+jaegarpoon@users.noreply.github.com>
2022-03-31 20:48:56 +0800

    Create DB class

---
+++ app/db.py
@@ -0,0 +1,32 @@
+from django.db import connection
+from datetime import datetime
+
+class DB:
+    def __init__(self):
+        self.cursor = connection.cursor()
+    
+    def get_venues(self):
+        self.cursor.execute("SELECT * FROM venues")
+        venues = self.cursor.fetchall()
+
+        return {'records': venues}
+    
+    def get_bbq_schedule(self):
+        availtimes = []
+        curryear = 2022
+        currmonth = 2
+        currdate = 2
+        starttime = 8
+        endtime = 22
+        
+        self.cursor.execute("SELECT eventstartdate FROM bookings WHERE venue='BBQ Pit'")
+        fetched = self.cursor.fetchall()
+        
+        unavailtimes = list(sum(fetched, ()))
+        
+        for i in range(endtime - starttime):
+            startstamp = datetime(curryear, currmonth, currdate, i + starttime, 0)
+            endstamp = datetime(curryear, currmonth, currdate, i + starttime + 1, 0)
+            availtimes.append([startstamp, endstamp, startstamp in unavailtimes])
+        
+        return {'available': availtimes}
